f6dc694cffeac46cd1de8d3d95820a4d61700aee
divyendu12 <divyendu12@gmail.com>
2014-12-11 16:28:33 +0530

    Deployed Selenium Project

---
+++ src/test/java/PetStore/TestLaunch/JPetStoreTest.java
@@ -0,0 +1,44 @@
+package PetStore.TestLaunch;
+
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+
+
+ 
+public class JPetStoreTest {
+ 
+	@Test
+    public static void TestLaunch () {
+        
+    	
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://127.0.0.1:8081/JpetStoreApplication/";
+        String expectedTitle = "Welcome to JPetStore 6";
+        String actualTitle = "";
+ 
+        
+        driver.get(baseUrl);
+ 
+        
+        actualTitle = driver.getTitle();
+ 
+        
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+        
+       
+        driver.close();
+        
+        
+       
+    }
+ 
+}
